983d23fc873087a7b3527f6138ca5dcd23de40ac
오세진 <jinioh88@gmail.com>
2018-08-09 18:10:10 +0900

    jpa

--- myshop/src/main/java/com/example/myshop/domain/Member.java
+++ myshop/src/main/java/com/example/myshop/domain/Member.java
@@ -2,11 +2,20 @@ package com.example.myshop.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
 @Setter @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @ToString
-public class Member {
+public class Member implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
